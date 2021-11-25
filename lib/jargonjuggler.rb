5cf8990787e7a0054151c99843fa0d72c0443fae
Brian Fundakowski Feldman <Brian.Fundakowski.Feldman@gmail.com>
2021-11-24 18:58:40 -0800

    Rename package and get basics going.

---
+++ lib/jargonjuggler.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require "twitch/bot"
+require "dotenv/load"
+require_relative "jargonjuggler/version"
+require_relative "jargonjuggler/join_channel_handler"
+require_relative "jargonjuggler/channel_command_handler"
+
+module JargonJuggler
+  class Error < StandardError; end
+
+  class Bot
+    def initialize
+      @client = Twitch::Bot::Client.new(
+        channel: ENV["TWITCH_CHANNEL"],
+        config: configuration,
+      ) do
+        register_handler(JargonJuggler::JoinChannelHandler)
+        register_handler(JargonJuggler::ChannelCommandHandler)
+      end
+    end
+
+    def run
+      client.run
+    end
+
+    private
+
+    attr_reader :client
+
+    def adapter_class
+      if development_mode?
+        "Twitch::Bot::Adapter::Terminal"
+      else
+        "Twitch::Bot::Adapter::Irc"
+      end
+    end
+
+    def configuration
+      Twitch::Bot::Config.new(
+        settings: {
+          botname: ENV["TWITCH_USERNAME"],
+          irc: {
+            nickname: ENV["TWITCH_USERNAME"],
+            password: ENV["TWITCH_PASSWORD"],
+          },
+          adapter: adapter_class,
+          memory: "Twitch::Bot::Memory::Hash",
+          log: {
+            file: logfile,
+            level: loglevel,
+          },
+        },
+      )
+    end
+
+    def logfile
+      if ENV["BOT_LOGFILE"]
+        File.new(ENV["BOT_LOGFILE"], "w")
+      else
+        STDOUT
+      end
+    end
+
+    def loglevel
+      (ENV["BOT_LOGLEVEL"] || "info").to_sym
+    end
+
+    def development_mode?
+      ENV["BOT_MODE"] == "development"
+    end
+  end
+end
---
+++ lib/jargonjuggler/channel_command_handler.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module JargonJuggler
+  # This class implements the !plan command.
+  class ChannelCommandHandler < Twitch::Bot::EventHandler
+    def self.handled_events
+      [:user_message]
+    end
+
+    def call
+      if event.command? && command_aliases.include?(event.command)
+        handle_plan_command
+      end
+    end
+
+    private
+
+    PLAN_FILE = File.expand_path("~/.plan")
+
+    def command_aliases
+      %w[plan project]
+    end
+
+    def handle_plan_command
+      args = event.command_args
+      subcommand = args.shift
+      if subcommand == "set"
+        if update_allowed?
+          plan = args.join(" ")
+          update_plan(plan)
+        else
+          client.send_message "Permission denied."
+        end
+      else
+        announce_plan
+      end
+    end
+
+    def update_allowed?
+      event.user == client.channel.name
+    end
+
+    def update_plan(plan)
+      client.memory.store("plan", plan)
+      client.send_message "Plan set to '#{plan}'"
+    end
+
+    def announce_plan
+      plan = client.memory.retrieve("plan")
+      client.send_message "#{client.channel.name}'s plan: #{plan}"
+    end
+
+    def plan_file_content
+      if File.exist?(PLAN_FILE)
+        File.read(PLAN_FILE)
+      else
+        "Error: Plan file missing!"
+      end
+    end
+  end
+end
